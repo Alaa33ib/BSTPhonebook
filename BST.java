44741b175c28ccf091f69d260af82a4bdaeff326
Alaa33ib <147752655+Alaa33ib@users.noreply.github.com>
2023-11-16 12:34:45 +0300

    Update BST.java
    remove n stuff

--- BST.java
+++ BST.java
@@ -76,3 +76,53 @@ public class BST //LinkedList of Contacts
 	  }
 	}
     
+  public boolean removeKey(String key)
+  {
+    Boolean removed = new Boolean(false);
+
+    BSTNode p;
+
+    p = removeAux(key, root, removed);
+
+    current = root = p;
+    return removed; //idk mate
+  }
+
+  public BSTNode removeAux(String key, BSTNode p, Boolean flag)
+  {
+    BSTNode q, child = null;
+    if (p==null) return null;
+    if (key.compareTo(p.getKey())==-1) p.setLeft(removeAux(key,p.getLeft(),flag));
+    else if (key.compareTo(p.getKey())==1) p.setRight(removeAux(key,p.getRight(),flag));
+    else 
+    {
+      flag.set(true);
+      if (p.getLeft()!=null && p.getRight()!=null)
+      {
+        q = findMin(p.getRight());
+        p.setKey(q.getKey());
+        p.setData(q.getData());
+        p.setRight(removeAux(q.getKey(), p.getRight(), flag));
+      }
+      else 
+      {
+      if (p.getRight()==null) child = p.getLeft();
+      else if (p.getLeft()==null) child = p.getRight();
+      return child;
+      }
+    }
+    return p;
+
+  }
+
+  private BSTNode findMin(BSTNode p)
+  {
+    if(p==null) return null;
+    while(p.getLeft()!=null)
+    {
+      p = p.getLeft();
+    }
+    return p;
+  }
+    
+
