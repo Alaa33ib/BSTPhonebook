6c6f2f9daab727c8d5da7d956f1240e0f7573a45
Alaa33ib <147752655+Alaa33ib@users.noreply.github.com>
2023-11-16 10:48:50 +0300

    Update BSTNode.java
    added compareTo

--- BSTNode.java
+++ BSTNode.java
@@ -18,7 +18,29 @@ public class BSTNode //Nodes that contain Contact as data
       left = l;
       key = k;
    }
-   
+
+    public int compareTo(Object s) //compares contact names alphabetically
+   {
+    Contact str = (Contact)s;
+    int firstLength = this.getData().getName().length();  
+    int secondLength = str.getData().getName().length();  
+    int minLength = Math.min(firstLength, secondLength);
+    int i = 0;  
+    while(i < minLength) 
+    {  
+     char ch1 = this.getDate().getName().charAt(i);  
+     char ch2 = str.getData().getName().charAt(i);  
+     if(ch1 != ch2) 
+     {  
+      if((int)(ch1-ch2) > 0)
+       return 1;
+      else
+       return -1;
+     }
+     i++;  
+   }  
+   return 0;  
+   } 
    
    public Contact getData()
    {
