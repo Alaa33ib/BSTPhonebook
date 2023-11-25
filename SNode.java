effb5893c1c60b7b8f6e362ac931882b1a726133
layalalt <147754472+layalalt@users.noreply.github.com>
2023-11-26 01:58:56 +0300

    deleted

--- SNode.java
+++ SNode.java
@@ -1,18 +1,1 @@
-public class SNode<T>
-{ 
-  public T data; 
-  public SNode next;
-   
-  public SNode() 
-  { 
-    data = null; 
-    next = null;
-  }
-  
-  public SNode(T val) 
-  { 
-    data = val;
-    next = null; 
-  }
- // Setters/Getters?
-}
+//deleted
